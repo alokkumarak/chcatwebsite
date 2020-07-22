6f4534ff8a709666a9864546975eb76557b38a0f
AleksandraZhi <jiterewa@gmail.com>
2020-07-22 19:31:57 +0100

    add templating

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,11 +2,15 @@ const express = require("express");
 const router = express.Router();
 const mainController = require("../controllers/mainController");
 
+<<<<<<< HEAD
 // router.get("/", mainController.homePage);
 
 router.get('/', (req, res) => {
   res.render('index');
 });
+=======
+router.get("/", mainController.novelsPage);
+>>>>>>> b00a69c... add templating
 
 router.get("/novels-rockstar", (req, res) => {
   res.render("layout");
